f018dd928760c781cae886ecb1f4947ee690f912
angelasofiaremolinagutierrez <angela.remolina.2019@upb.edu.co>
2021-10-05 18:37:31 -0500

    Sniffer Linux

---
+++ Proyecto/networking/ethernet.py
@@ -0,0 +1,23 @@
+import socket
+import struct
+
+# Returns MAC as string from bytes (ie AA:BB:CC:DD:EE:FF)
+def get_mac_addr(mac_raw):
+    byte_str = map('{:02x}'.format, mac_raw)
+    mac_addr = ':'.join(byte_str).upper()
+    return mac_addr
+
+class Ethernet:
+
+    def __init__(self, raw_data):
+
+        dest, src, prototype = struct.unpack('! 6s 6s H', raw_data[:14])
+
+        self.dest_mac = get_mac_addr(dest)
+        self.src_mac = get_mac_addr(src)
+        self.proto = socket.htons(prototype)
+        self.data = raw_data[14:]
+    
+   
+
+
