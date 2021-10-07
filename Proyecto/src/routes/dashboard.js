b877b4f3e3a1fb3e9ae6e28fc3cf8f2a77102bae
angelasofiaremolinagutierrez <angela.remolina.2019@upb.edu.co>
2021-10-06 19:42:49 -0500

    flask connection between python and js

--- Proyecto/src/routes/dashboard.js
+++ Proyecto/src/routes/dashboard.js
@@ -2,21 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const pool = require('../database');
-const {isLoggedIn, isNotLoggedIn} = require('../lib/auth');
+const { isLoggedIn, isNotLoggedIn } = require('../lib/auth');
 
 //Only Dahsboard
-router.get('/dashboard', isLoggedIn, async(req, res) => {
+router.get('/dashboard', isLoggedIn, async (req, res) => {
     res.render('dashboard/dashboard');
 });
 
 //Dahsboard users
 
-router.get('/dashboard/users', isLoggedIn, async(req, res) => {
+router.get('/dashboard/users', isLoggedIn, async (req, res) => {
     const users = await pool.query('SELECT * FROM users');// It send to the list and create an array with the users
-    res.render('dashboard/users/list',{ users: users});
+    res.render('dashboard/users/list', { users: users });
 });
 
-router.get('/dashboard/users/edit/:id',isLoggedIn, async (req, res) => {
+router.get('/dashboard/users/edit/:id', isLoggedIn, async (req, res) => {
     const { id } = req.params;
     const users = await pool.query('SELECT * FROM users WHERE id = ?', [id]);
     res.render('./dashboard/users/edit', { user: users[0] });
@@ -37,7 +37,7 @@ router.post('/dashboard/users/edit/:id', isLoggedIn, async (req, res) => {
     res.redirect('/dashboard/users');
 });
 
-router.get('/dashboard/users/delete/:id',isLoggedIn, async (req, res) => {
+router.get('/dashboard/users/delete/:id', isLoggedIn, async (req, res) => {
     const { id } = req.params;
     await pool.query('DELETE FROM links WHERE user_id = ?', [id]);
     await pool.query('DELETE FROM users WHERE ID = ?', [id]);
@@ -50,7 +50,7 @@ router.get('/dashboard/users/add', isLoggedIn, (req, res) => {
 });
 
 router.post('/dashboard/users/add', isLoggedIn, async (req, res) => {
-    const { fullname, username, email, role, status,password} = req.body;
+    const { fullname, username, email, role, status, password } = req.body;
     const newUser = {
         fullname,
         username,
@@ -63,4 +63,29 @@ router.post('/dashboard/users/add', isLoggedIn, async (req, res) => {
     req.flash('success', 'User saved successfully');
     res.redirect('/dashboard/users');
 });
+
+
+// Make a net capture (sniff)
+
+router.get('/dashboard/captures/add', isLoggedIn, (req, res) => {
+    // TO DO: VIEW OF CAPTURES IN DASHBOARD
+    // res.render('dashboard/captures/add');
+});
+
+router.post('/dashboard/captures', isLoggedIn, async (req, res) => {
+
+    fetch('http://127.0.0.1:5000/sniff')
+        .then((response) => {
+            return response.json();
+        })
+    
+    //TODO: STRUCTURE AND SAVE CAPTURE
+    
+    //await pool.query('INSERT INTO captures set ?', [newUser]);
+    //req.flash('success', 'Capture made succesfully!');
+    //res.redirect('/dashboard/');
+});
+
+
+
 module.exports = router;
