ba3cc7cc71dde982827aab333456b5ae2187b1ec
angelasofiaremolinagutierrez <angela.remolina.2019@upb.edu.co>
2021-10-18 20:47:34 -0700

    graphics 1 and 2 done

--- Proyecto/src/routes/statistics.js
+++ Proyecto/src/routes/statistics.js
@@ -5,8 +5,7 @@ const pool = require('../database');
 const helpers = require('../lib/helpers');
 const { isLoggedIn, isNotLoggedIn } = require('../lib/auth');
 
-router.get('/statistics', isLoggedIn, async (req, res) => {
-    const { id } = req.params
+router.get('/statistics/graphic1', isLoggedIn, async (req, res) => {
     const data_bar_graph = await pool.query('SELECT email, count(*) as count FROM capture, user WHERE user_id_u = id_u GROUP BY user_id_u');
     var counts = data_bar_graph
     var users_list = []
@@ -19,8 +18,30 @@ router.get('/statistics', isLoggedIn, async (req, res) => {
 
     console.log(users_list);
     console.log(counts_list);
-    
-    res.render("dashboard/statistics/graphics",{users_list,counts_list});
+
+    res.render("dashboard/statistics/graphic1", { users_list, counts_list });
+});
+
+router.get('/statistics/graphic2', isLoggedIn, async (req, res) => {
+    const data_pie_graph = await pool.query('SELECT capture_id_c, count(*) as count FROM frame GROUP BY capture_id_c');
+    var counts = data_pie_graph
+    var id_list = []
+    var counts_list = []
+    for (let i = 0; i < counts.length; i++) {
+        const element = counts[i];
+        id_list.push(element.capture_id_c)
+        counts_list.push(element.count)
+    }
+
+    console.log(id_list);
+    console.log(counts_list);
+
+    res.render("dashboard/statistics/graphic2", {id_list, counts_list});
+
+});
+
+router.get('/statistics/graphic3', isLoggedIn, async (req, res) => {
+
 });
 
 module.exports = router;
